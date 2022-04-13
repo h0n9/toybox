17671c0378a07d98bbfc3aa231eb10314cf6f130
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-04-13 16:24:40 +0900

    Get rid of dumb-init

--- kistio/sample-client/DOCKER/docker-entrypoint.sh
+++ kistio/sample-client/DOCKER/docker-entrypoint.sh
@@ -1,27 +1,32 @@
-#!/usr/bin/dumb-init /bin/sh
+#!/bin/sh
 
-FLAGS=
+if [ "${1}" == "serve" ]; then
+    FLAGS=
 
-if [ ! -z "$AGENT_HOST" ]; then
-    FLAGS="$FLAGS --agent-host $AGENT_HOST"
+    if [ ! -z "$AGENT_HOST" ]; then
+        FLAGS="$FLAGS --agent-host $AGENT_HOST"
+    fi
+
+    if [ ! -z "$AGENT_PORT" ]; then
+        FLAGS="$FLAGS --agent-port $AGENT_PORT"
+    fi
+
+    if [ ! -z "$TOPIC_PUB" ]; then
+        FLAGS="$FLAGS --topic-pub $TOPIC_PUB"
+    fi
+
+    if [ ! -z "$TOPIC_SUB" ]; then
+        FLAGS="$FLAGS --topic-sub $TOPIC_SUB"
+    fi
+
+    if [ ! -z "$INTERVAL" ]; then
+        FLAGS="$FLAGS --interval $INTERVAL"
+    fi
+
+    echo $FLAGS
+
+    exec /usr/bin/app/sample-client $FLAGS
+
+else
+    exec ${@}
 fi
-
-if [ ! -z "$AGENT_PORT" ]; then
-    FLAGS="$FLAGS --agent-port $AGENT_PORT"
-fi
-
-if [ ! -z "$TOPIC_PUB" ]; then
-    FLAGS="$FLAGS --topic-pub $TOPIC_PUB"
-fi
-
-if [ ! -z "$TOPIC_SUB" ]; then
-    FLAGS="$FLAGS --topic-sub $TOPIC_SUB"
-fi
-
-if [ ! -z "$INTERVAL" ]; then
-    FLAGS="$FLAGS --interval $INTERVAL"
-fi
-
-echo $FLAGS
-
-/usr/bin/app/sample-client $FLAGS
