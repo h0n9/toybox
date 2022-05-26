1bb44286aae5326635df0385ffb4e0762aa67049
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-05-27 08:13:50 +0900

    Add go routines for keyevents, slowlogs

--- redis-logger/cmd/redis-logger/main.go
+++ redis-logger/cmd/redis-logger/main.go
@@ -3,45 +3,83 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/h0n9/toybox/redis-logger/util"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	DefaultServiceName   = "redis-logger"
+	DefaultRedisAddr     = "localhost:6379"
+	DefaultRedisPassword = ""
 )
 
 func main() {
+	// get envs
+	serviceName := util.GetEnv("SERVICE_NAME", DefaultServiceName)
+	addr := util.GetEnv("REDIS_ADDR", DefaultRedisAddr)
+	password := util.GetEnv("REDIS_PASSWORD", DefaultRedisPassword)
+
 	// init
 	ctx := context.Background()
+	wg := sync.WaitGroup{}
+	logger := log.With().
+		Str("service", serviceName).
+		Str("redis-addr", addr).
+		Logger()
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Addr:     addr,
+		Password: password,
 		DB:       0,
 	})
 	defer rdb.Close()
 
 	// test connection
-	fmt.Printf("PING -> ")
 	ping := rdb.Ping(ctx)
 	result, err := ping.Result()
 	if err != nil {
-		panic(err)
+		logger.Panic().Msg(err.Error())
 	}
-	fmt.Println(result)
+	logger.Info().Msg(result)
 
-	fmt.Printf("GET hello -> ")
-	result, err = rdb.Get(ctx, "hello").Result()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(result)
-	}
+	// subscribe keyevent
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 
-	pubsub := rdb.PSubscribe(ctx, "__key*__:*")
-	defer pubsub.Close()
+		pubsub := rdb.PSubscribe(ctx, "__keyevent*__:*")
+		defer pubsub.Close()
 
-	for {
-		msg, err := pubsub.ReceiveMessage(ctx)
-		if err != nil {
-			panic(err)
+		for {
+			msg, err := pubsub.ReceiveMessage(ctx)
+			if err != nil {
+				logger.Panic().Msg(err.Error())
+			}
+			logger.Info().Msg(msg.Channel)
 		}
-		fmt.Println(msg.String())
-	}
+	}()
+
+	// get slowlogs
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for {
+			slowlogs, err := rdb.SlowLogGet(ctx, 1).Result()
+			if err != nil {
+				logger.Err(err)
+				continue
+			}
+			if len(slowlogs) < 1 {
+				continue
+			}
+			logger.Info().Msg(fmt.Sprint(slowlogs[0].Args))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	wg.Wait()
 }
