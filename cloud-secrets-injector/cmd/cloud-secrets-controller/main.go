ce244c262f20c669105d56bd1c172bf2e0736c7f
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-06-20 21:57:27 +0900

    Fill up main() func

--- cloud-secrets-injector/cmd/cloud-secrets-controller/main.go
+++ cloud-secrets-injector/cmd/cloud-secrets-controller/main.go
@@ -7,6 +7,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 func init() {
@@ -17,11 +19,22 @@ func main() {
 	// init logger
 	logger := log.Log.WithName("cloud-secrets-controller")
 
-	_, err := manager.New(config.GetConfigOrDie(), manager.Options{
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
 		Logger: logger,
 	})
 	if err != nil {
-		logger.Error(err, "unable to se up overall controller manager")
+		logger.Error(err, "faild to setup overall controller manager")
+		os.Exit(1)
+	}
+
+	hookServer := mgr.GetWebhookServer()
+
+	hookServer.Register("/mutate", &webhook.Admission{Handler: nil})
+	hookServer.Register("/validate", &webhook.Admission{Handler: nil})
+
+	err = mgr.Start(signals.SetupSignalHandler())
+	if err != nil {
+		logger.Error(err, "failed to run manager")
 		os.Exit(1)
 	}
 }
