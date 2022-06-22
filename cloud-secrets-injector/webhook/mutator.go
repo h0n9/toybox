fc54e0be9e8bff780fd9e07125ed60f7db1e151e
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-06-22 09:12:28 +0900

    Add a little big of mutator handle logic

--- cloud-secrets-injector/webhook/mutator.go
+++ cloud-secrets-injector/webhook/mutator.go
@@ -2,7 +2,9 @@ package webhook
 
 import (
 	"context"
+	"net/http"
 
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
@@ -13,6 +15,18 @@ type Mutator struct {
 }
 
 func (mutator *Mutator) Handle(ctx context.Context, req admission.Request) admission.Response {
+	pod := &corev1.Pod{}
+
+	err := mutator.decoder.Decode(req, pod)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	annotations := pod.Annotations
+	if len(annotations) == 0 {
+		return admission.Allowed("found no cloud-secrets-injector related annotations")
+	}
+
 	return admission.Response{}
 }
 
