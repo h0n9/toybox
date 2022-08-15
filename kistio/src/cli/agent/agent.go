d3795509e0448ec9aed79d2ee7c530d060ec2514
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-08-15 13:03:18 +0900

    Add standard Run func()

--- kistio/src/cli/agent/agent.go
+++ kistio/src/cli/agent/agent.go
@@ -1,6 +1,18 @@
 package agent
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+
+	kistio "github.com/h0n9/toybox/kistio/src"
+)
 
 var Cmd = &cobra.Command{
 	Use:   "agent",
@@ -10,6 +22,29 @@ var Cmd = &cobra.Command{
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "run a server for connector node",
+	Run: func(cmd *cobra.Command, args []string) {
+		logger := zerolog.New(os.Stderr).With().
+			Timestamp().
+			Str("service", fmt.Sprintf("%s-%s", kistio.Name, Cmd.Use)).
+			Logger()
+		_, cancel := context.WithCancel(context.Background())
+		sigCh := make(chan os.Signal, 1)
+		defer close(sigCh)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+		wg := sync.WaitGroup{}
+
+		logger.Info().Msg("initialized logger, context, sig channel, wait group")
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			sig := <-sigCh
+			logger.Info().Msg("receieved signal " + sig.String())
+			cancel()
+		}()
+
+		wg.Wait()
+	},
 }
 
 func init() {
--- kistio/src/go.mod
+++ kistio/src/go.mod
@@ -6,5 +6,9 @@ require github.com/spf13/cobra v1.5.0
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/rs/zerolog v1.27.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
