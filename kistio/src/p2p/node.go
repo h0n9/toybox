8ed3cd3eae9770d26d73aa9fbf1486259a4c5428
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-08-17 22:26:15 +0900

    Add logic for creating p2p host

--- kistio/src/p2p/node.go
+++ kistio/src/p2p/node.go
@@ -3,8 +3,14 @@ package p2p
 import (
 	"bytes"
 	"context"
+	"fmt"
 
+	libp2p "github.com/libp2p/go-libp2p"
+	libp2pHost "github.com/libp2p/go-libp2p-core/host"
+	quic "github.com/libp2p/go-libp2p-quic-transport"
 	"github.com/postie-labs/go-postie-lib/crypto"
+
+	"github.com/h0n9/toybox/kistio/src/util"
 )
 
 type Node struct {
@@ -13,9 +19,12 @@ type Node struct {
 	privKey *crypto.PrivKey
 	pubKey  *crypto.PubKey
 	addr    crypto.Addr
+
+	host libp2pHost.Host
 }
 
-func NewNode(ctx context.Context, seed []byte) (*Node, error) {
+func NewNode(ctx context.Context, seed []byte, listenAddrs crypto.Addrs) (*Node, error) {
+	// generate private key
 	privKey, err := crypto.GenPrivKey()
 	if !bytes.Equal(seed, []byte{}) {
 		privKey, err = crypto.GenPrivKeyFromSeed(seed)
@@ -23,10 +32,52 @@ func NewNode(ctx context.Context, seed []byte) (*Node, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// create listenAddr if no received listenAddrs
+	if len(listenAddrs) == 0 {
+		listenAddr, err := crypto.NewMultiAddr(
+			fmt.Sprintf("%s/%d/%s",
+				DefaultListenAddr,
+				util.GenRandomInt(ListenPortMax, ListenPortMin),
+				TransportProtocol,
+			),
+		)
+		if err != nil {
+			return nil, err
+		}
+		listenAddrs = append(listenAddrs, listenAddr)
+	}
+
+	// transform privKey to ecdsa based p2p privKey
+	privKeyP2P, err := privKey.ToECDSAP2P()
+	if err != nil {
+		return nil, err
+	}
+
+	// init quic transport layer
+	quicTransport, err := quic.NewTransport(privKeyP2P, nil, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// create libp2p host
+	host, err := libp2p.New(
+		libp2p.ListenAddrs(listenAddrs...),
+		libp2p.Identity(privKeyP2P),
+		libp2p.Transport(quicTransport),
+		libp2p.DefaultSecurity,
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Node{
-		ctx:     ctx,
+		ctx: ctx,
+
 		privKey: privKey,
 		pubKey:  privKey.PubKey(),
 		addr:    privKey.PubKey().Address(),
+
+		host: host,
 	}, nil
 }
