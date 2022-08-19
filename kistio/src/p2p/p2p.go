7315249b667571dc0cd7692233cfe6ff5b86c4ac
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-08-19 12:49:02 +0900

    Add missing p2p/p2p.go

---
+++ kistio/src/p2p/p2p.go
@@ -0,0 +1,10 @@
+package p2p
+
+const (
+	TransportProtocol = "quic"
+	ProtocolID        = "/kistio-agent/1.0.0"
+
+	DefaultListenAddr = "/ip4/0.0.0.0/udp"
+	ListenPortMin     = 49152
+	ListenPortMax     = 65535
+)
