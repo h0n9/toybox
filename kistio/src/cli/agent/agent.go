e8da09baa7a69fd0b974fdf56a60d8b534d40330
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-09-06 07:56:23 +0900

    Add basic logic for agent server

--- kistio/src/cli/agent/agent.go
+++ kistio/src/cli/agent/agent.go
@@ -8,10 +8,19 @@ import (
 	"sync"
 	"syscall"
 
+	"github.com/postie-labs/go-postie-lib/crypto"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 
 	kistio "github.com/h0n9/toybox/kistio/src"
+	"github.com/h0n9/toybox/kistio/src/p2p"
+)
+
+var (
+	seed           string
+	listenAddrs    crypto.Addrs
+	bootstrapAddrs crypto.Addrs
+	rendezVous     string
 )
 
 var Cmd = &cobra.Command{
@@ -23,6 +32,10 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "run a server for connector node",
 	Run: func(cmd *cobra.Command, args []string) {
+		var (
+			node *p2p.Node
+		)
+
 		// init logger
 		logger := zerolog.New(os.Stderr).With().
 			Timestamp().
@@ -30,7 +43,8 @@ var runCmd = &cobra.Command{
 			Logger()
 
 		// init context
-		_, cancel := context.WithCancel(context.Background())
+		ctx, cancel := context.WithCancel(context.Background())
+		ctx = context.WithValue(ctx, "logger", logger)
 
 		// init sig channel
 		sigCh := make(chan os.Signal, 1)
@@ -49,11 +63,47 @@ var runCmd = &cobra.Command{
 			sig := <-sigCh
 			logger.Info().Msg("receieved signal " + sig.String())
 			cancel()
+			if node != nil {
+				node.Close()
+			}
 		}()
 
 		// **********************
 		// * do something here *
 		// **********************
+
+		// transform seed string to byte slice if not ""
+		seedBytes := []byte{}
+		if seed != "" {
+			seedBytes = []byte(seed)
+		}
+
+		// init node
+		node, err := p2p.NewNode(ctx, seedBytes, listenAddrs, false)
+		if err != nil {
+			logger.Err(err).Msg("")
+			return
+		}
+		logger.Info().Msgf("initialized node: %s", node.GetAddr())
+
+		// bootstrap node
+		err = node.Bootstrap(bootstrapAddrs...)
+		if err != nil {
+			logger.Err(err).Msg("")
+			return
+		}
+		logger.Info().Msg("bootstrapped peer nodes")
+
+		// discover peers
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := node.Discover(rendezVous)
+			if err != nil {
+				logger.Err(err).Msg("")
+				return
+			}
+		}()
 
 		// wait until all of wait groups are done
 		wg.Wait()
@@ -61,5 +111,9 @@ var runCmd = &cobra.Command{
 }
 
 func init() {
+	runCmd.Flags().StringVarP(&seed, "seed", "s", "", "seed for private key")
+	runCmd.Flags().VarP(&listenAddrs, "listen", "l", "listening addresses")
+	runCmd.Flags().VarP(&bootstrapAddrs, "bootstrap", "b", "bootstrap address")
+	runCmd.Flags().StringVar(&rendezVous, "rendez-vous", "", "rendez-vous point")
 	Cmd.AddCommand(runCmd)
 }
