50f652254f8a36a3ca326fc6da06b56c84d736ec
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-12-06 08:08:18 +0900

    Add very first LakeServer

---
+++ msg-lake/lake/lake.go
@@ -0,0 +1,59 @@
+package lake
+
+import (
+	"container/list"
+	"fmt"
+
+	pb "github.com/h0n9/toybox/msg-lake/proto"
+)
+
+type LakeServer struct {
+	pb.UnimplementedLakeServer
+
+	msgs map[*pb.ID]*list.List
+}
+
+func NewLakeServer() *LakeServer {
+	return &LakeServer{
+		msgs: map[*pb.ID]*list.List{},
+	}
+}
+
+func (ls *LakeServer) Close() {
+	// TODO: implement Close() method
+}
+
+func (ls *LakeServer) Send(req *pb.SendReq) (*pb.SendRes, error) {
+	id := req.GetId()
+	msg := req.GetMsg()
+
+	if _, exist := ls.msgs[id]; !exist {
+		ls.msgs[id] = list.New()
+	}
+
+	ls.msgs[id].PushBack(msg)
+
+	return &pb.SendRes{Ok: true}, nil
+}
+
+func (ls *LakeServer) Recv(req *pb.RecvReq, stream pb.Lake_RecvServer) error {
+	id := req.GetId()
+
+	msgs, exist := ls.msgs[id]
+	if !exist {
+		return fmt.Errorf("failed to find msgs corresponding to id(%s)", req.GetId())
+	}
+
+	for msgs.Len() > 0 {
+		front := msgs.Front()
+		msg := front.Value.(*pb.Msg)
+		err := stream.Send(&pb.RecvRes{Msg: msg})
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		msgs.Remove(front)
+	}
+
+	return nil
+}
