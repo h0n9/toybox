e1e8f03ec8875375da6dba0b2c98aed474db4e48
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-12-13 21:10:06 +0900

    Add memory_test.go

---
+++ msg-lake/store/memory_test.go
@@ -0,0 +1,44 @@
+package store
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"testing"
+
+	"github.com/h0n9/toybox/msg-lake/proto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMsgStoreMemory(t *testing.T) {
+	// const
+	const (
+		N  int    = 1000
+		ID string = "test"
+	)
+
+	ms := NewMsgStoreMemory()
+	assert.Equal(t, 0, len(ms.msgs))
+	assert.Equal(t, 0, ms.Len(ID))
+
+	for i := 0; i < N; i++ {
+		hash := sha256.Sum256([]byte(strconv.Itoa(rand.Int())))
+		ms.Push(ID, &proto.Msg{
+			From: &proto.Address{Address: fmt.Sprintf("addr-%d", i)},
+			Data: &proto.Data{Data: hash[:]},
+		})
+	}
+
+	assert.Equal(t, 1, len(ms.msgs))
+	assert.Equal(t, N, ms.Len(ID))
+
+	for i := 0; i < N; i++ {
+		msg, err := ms.Pop(ID)
+		assert.NoError(t, err)
+		assert.Equal(t, fmt.Sprintf("addr-%d", i), msg.GetFrom().GetAddress())
+	}
+
+	assert.Equal(t, 1, len(ms.msgs))
+	assert.Equal(t, 0, ms.Len(ID))
+}
