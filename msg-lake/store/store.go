2dcf2795e6cd19dffad82fde6e1b614722552ebe
h0n9 <monsieur.ahn@gmail.com>
2023-01-28 18:12:47 +0900

    Minor changes

--- msg-lake/store/store.go
+++ msg-lake/store/store.go
@@ -3,12 +3,14 @@ package store
 import (
 	"sync"
 
-	"github.com/h0n9/toybox/msg-lake/proto"
+	pb "github.com/h0n9/toybox/msg-lake/proto"
 )
 
 type MsgStore interface {
 	Produce(msgBoxID string) (*sync.Map, error)
-	Consume(msgBoxID, consumerID string) (<-chan *proto.MsgCapsule, error)
+	Consume(msgBoxID, consumerID string) (<-chan *pb.MsgCapsule, error)
 	Sync(msgBoxID, consumerID string) error
 	Stop(msgBoxID, consumerID string) error
 }
+
+type MsgCapsuleChan chan *pb.MsgCapsule
