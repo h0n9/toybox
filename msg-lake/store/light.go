58b0a58ed67eb9a5c73d494ef42e186afe736d78
h0n9 <monsieur.ahn@gmail.com>
2023-01-30 21:41:12 +0900

    Re-factor MsgBoxLight

--- msg-lake/store/light.go
+++ msg-lake/store/light.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"context"
 	"fmt"
 	"sync"
 
@@ -8,53 +9,141 @@ import (
 )
 
 type MsgStoreLight struct {
+	ctx      context.Context
 	msgBoxes *sync.Map // <msg_box_id>:<msg_box>
 }
 
-func NewMsgStoreLight() *MsgStoreLight {
+func NewMsgStoreLight(ctx context.Context) *MsgStoreLight {
 	return &MsgStoreLight{
+		ctx:      ctx,
 		msgBoxes: &sync.Map{},
 	}
 }
 
 func (store *MsgStoreLight) GetMsgBox(msgBoxID string) *MsgBoxLight {
-	value, _ := store.msgBoxes.LoadOrStore(msgBoxID, NewMsgBoxLight())
-	return value.(*MsgBoxLight)
+	value, exist := store.msgBoxes.Load(msgBoxID)
+	if exist {
+		return value.(*MsgBoxLight)
+	}
+	msgBoxLight := NewMsgBoxLight()
+	store.msgBoxes.Store(msgBoxID, msgBoxLight)
+	go msgBoxLight.Relay(store.ctx)
+	return msgBoxLight
+}
+
+type SetConsumerChan struct {
+	consumerID   string
+	consumerChan MsgCapsuleChan
 }
 
 type MsgBoxLight struct {
-	msgCapsuleChans *sync.Map // <consumer_id>:<msg_capsule_chan>
+	isRelaying bool
+
+	// chans for operations
+	setConsumerChan   chan SetConsumerChan
+	closeConsumerChan chan string
+
+	// chans for msgs
+	producerChan  MsgCapsuleChan
+	consumerChans map[string]MsgCapsuleChan
 }
 
 func NewMsgBoxLight() *MsgBoxLight {
-	return &MsgBoxLight{msgCapsuleChans: &sync.Map{}}
-}
+	return &MsgBoxLight{
+		isRelaying: false,
 
-func (box *MsgBoxLight) CreateMsgCapsuleChan(consumerID string) (MsgCapsuleChan, error) {
-	if _, exist := box.msgCapsuleChans.Load(consumerID); exist {
-		return nil, fmt.Errorf("found existing consumer chan for consumer id(%s)", consumerID)
+		setConsumerChan:   make(chan SetConsumerChan, 1),
+		closeConsumerChan: make(chan string, 1),
+
+		producerChan:  make(MsgCapsuleChan, 1),
+		consumerChans: make(map[string]MsgCapsuleChan),
 	}
-	msgCapsuleChan := make(MsgCapsuleChan)
-	box.msgCapsuleChans.Store(consumerID, msgCapsuleChan)
-	return msgCapsuleChan, nil
 }
 
-func (box *MsgBoxLight) RemoveMsgCapsuleChan(consumerID string) error {
-	box.msgCapsuleChans.Delete(consumerID)
-	return nil
+func (box *MsgBoxLight) GetProducerChan() MsgCapsuleChan {
+	return box.producerChan
 }
 
-func (box *MsgBoxLight) GetMsgCapsuleChan(consumerID string) (MsgCapsuleChan, error) {
-	value, exist := box.msgCapsuleChans.Load(consumerID)
-	if !exist {
-		return nil, fmt.Errorf("failed to find consumer chan for consumer id(%s)", consumerID)
+func (box *MsgBoxLight) Relay(ctx context.Context) {
+	var (
+		setConsumerChan   SetConsumerChan
+		closeConsumerChan string
+
+		consumerID   string
+		consumerChan MsgCapsuleChan
+		msgCapsule   *pb.MsgCapsule
+	)
+	if box.isRelaying {
+		return
 	}
-	return value.(MsgCapsuleChan), nil
+	box.isRelaying = true
+	for {
+		select {
+		// handling done ctx
+		case <-ctx.Done():
+			for consumerID, consumerChan = range box.consumerChans {
+				close(consumerChan)
+				delete(box.consumerChans, consumerID)
+			}
+			box.isRelaying = false
+			return
+
+		// handling operation: setConsumerChan
+		case setConsumerChan = <-box.setConsumerChan:
+			consumerID = setConsumerChan.consumerID
+			consumerChan = setConsumerChan.consumerChan
+			if _, exist := box.consumerChans[consumerID]; exist {
+				fmt.Printf("found existing consumer chan for consumer id(%s)", consumerID)
+				continue
+			}
+			box.consumerChans[consumerID] = consumerChan
+
+		// handling operation: closeConsumerChan
+		case closeConsumerChan = <-box.closeConsumerChan:
+			consumerID = closeConsumerChan
+			consumerChan, exist := box.consumerChans[consumerID]
+			if !exist {
+				fmt.Printf("failed to find consumer chan for consumer id(%s)", consumerID)
+				continue
+			}
+			close(consumerChan)
+			delete(box.consumerChans, consumerID)
+
+		// handling msg
+		case msgCapsule = <-box.producerChan:
+			for _, consumerChan = range box.consumerChans {
+				consumerChan <- msgCapsule
+			}
+		}
+	}
 }
 
-func (box *MsgBoxLight) SendMsgCapsule(msgCapsule *pb.MsgCapsule) {
-	box.msgCapsuleChans.Range(func(key, value any) bool {
-		value.(MsgCapsuleChan) <- msgCapsule
-		return true
-	})
-}
+// func (box *MsgBoxLight) CreateMsgCapsuleChan(consumerID string) (MsgCapsuleChan, error) {
+// 	if _, exist := box.msgCapsuleChans.Load(consumerID); exist {
+// 		return nil, fmt.Errorf("found existing consumer chan for consumer id(%s)", consumerID)
+// 	}
+// 	msgCapsuleChan := make(MsgCapsuleChan, 1)
+// 	box.msgCapsuleChans.Store(consumerID, msgCapsuleChan)
+// 	return msgCapsuleChan, nil
+// }
+//
+// func (box *MsgBoxLight) RemoveMsgCapsuleChan(consumerID string) error {
+// 	box.msgCapsuleChans.Delete(consumerID)
+// 	return nil
+// }
+//
+// func (box *MsgBoxLight) GetMsgCapsuleChan(consumerID string) (MsgCapsuleChan, error) {
+// 	value, exist := box.msgCapsuleChans.Load(consumerID)
+// 	if !exist {
+// 		return nil, fmt.Errorf("failed to find consumer chan for consumer id(%s)", consumerID)
+// 	}
+// 	return value.(MsgCapsuleChan), nil
+// }
+//
+// func (box *MsgBoxLight) SendMsgCapsule(msgCapsule *pb.MsgCapsule) {
+// 	box.msgCapsuleChans.Range(func(key, value any) bool {
+// 		value.(MsgCapsuleChan) <- msgCapsule
+// 		return true
+// 	})
+// }
+//
