83ac7f4630164b4b09df9989a44ab39c04317ad4
h0n9 <monsieur.ahn@gmail.com>
2023-02-01 20:34:13 +0900

    Clean up

--- msg-lake/store/light.go
+++ msg-lake/store/light.go
@@ -86,7 +86,6 @@ func (box *MsgBoxLight) CloseConsumerChan(consumerID string, errorChan chan erro
 }
 
 func (box *MsgBoxLight) Relay(ctx context.Context) {
-	fmt.Println("relaying")
 	var (
 		setConsumerChan   setConsumerChan
 		closeConsumerChan closeConsumerChan
@@ -142,33 +141,3 @@ func (box *MsgBoxLight) Relay(ctx context.Context) {
 		}
 	}
 }
-
-// func (box *MsgBoxLight) CreateMsgCapsuleChan(consumerID string) (MsgCapsuleChan, error) {
-// 	if _, exist := box.msgCapsuleChans.Load(consumerID); exist {
-// 		return nil, fmt.Errorf("found existing consumer chan for consumer id(%s)", consumerID)
-// 	}
-// 	msgCapsuleChan := make(MsgCapsuleChan, 1)
-// 	box.msgCapsuleChans.Store(consumerID, msgCapsuleChan)
-// 	return msgCapsuleChan, nil
-// }
-//
-// func (box *MsgBoxLight) RemoveMsgCapsuleChan(consumerID string) error {
-// 	box.msgCapsuleChans.Delete(consumerID)
-// 	return nil
-// }
-//
-// func (box *MsgBoxLight) GetMsgCapsuleChan(consumerID string) (MsgCapsuleChan, error) {
-// 	value, exist := box.msgCapsuleChans.Load(consumerID)
-// 	if !exist {
-// 		return nil, fmt.Errorf("failed to find consumer chan for consumer id(%s)", consumerID)
-// 	}
-// 	return value.(MsgCapsuleChan), nil
-// }
-//
-// func (box *MsgBoxLight) SendMsgCapsule(msgCapsule *pb.MsgCapsule) {
-// 	box.msgCapsuleChans.Range(func(key, value any) bool {
-// 		value.(MsgCapsuleChan) <- msgCapsule
-// 		return true
-// 	})
-// }
-//
