5627def01b473d5d6c63a53dcd529335197d273a
socar-monsieur <monsieur@socar.kr>
2023-02-04 16:20:43 +0900

    Refactor, simplify load test

--- msg-lake/cli/lab/test/test.go
+++ msg-lake/cli/lab/test/test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"bytes"
 	"context"
 	"crypto/tls"
 	"fmt"
@@ -16,10 +15,8 @@ import (
 	"github.com/postie-labs/go-postie-lib/crypto"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 
 	pb "github.com/h0n9/toybox/msg-lake/proto"
@@ -28,7 +25,6 @@ import (
 const (
 	DefaultTlsEnabled    = false
 	DefaultHostAddr      = "localhost:8080"
-	DefaultNumOfTopics   = 10
 	DefaultNumOfUsers    = 100
 	DefaultTopicLength   = 10
 	DefaultRandomEnabled = false
@@ -38,7 +34,6 @@ const (
 var (
 	tlsEnabled    bool
 	hostAddr      string
-	numOfTopics   int
 	numOfUsers    int
 	topicLength   int
 	randomEnabled bool
@@ -98,6 +93,11 @@ var Cmd = &cobra.Command{
 		// seed random
 		rand.Seed(time.Now().UnixNano())
 
+		msgBoxID := "topic-0"
+		if randomEnabled {
+			msgBoxID = fmt.Sprintf("topic-%d", rand.Int())
+		}
+
 		for i := 0; i < numOfUsers && loop; i++ {
 			userIndex := i
 			if randomEnabled {
@@ -120,119 +120,95 @@ var Cmd = &cobra.Command{
 			conns = append(conns, conn)
 			cli := pb.NewLakeClient(conn)
 
-			for j := 0; j < numOfTopics && loop; j++ {
-				topicIndex := j
-				if randomEnabled {
-					topicIndex = rand.Int()
+			// send
+			subWg.Add(1)
+			go func() {
+				defer subWg.Done()
+				ticker := time.NewTicker(1 * time.Second)
+				defer ticker.Stop()
+				sender, err := cli.Send(ctx)
+				if err != nil {
+					fmt.Println(err)
+					return
 				}
-				topic := fmt.Sprintf("topic-%d", topicIndex)
-				// execute goroutine (receiver)
-				subWg.Add(1)
-				go func() {
-					defer subWg.Done()
-					stream, err := cli.Recv(ctx, &pb.RecvReq{
-						MsgBoxId:   topic,
-						ConsumerId: nickname,
-					})
-					if err != nil {
-						fmt.Println(err)
+				for {
+					select {
+					case <-ctx.Done():
+						err = sender.CloseSend()
+						if err != nil {
+							fmt.Println(err)
+						}
 						return
-					}
-					for {
-						select {
-						case <-ctx.Done():
+					case <-ticker.C:
+						msg := &pb.Msg{
+							Data: []byte(fmt.Sprintf("helloworld-%d", time.Now().UnixNano())),
+							Metadata: map[string][]byte{
+								"nickname": []byte(nickname),
+							},
+						}
+						data, err := proto.Marshal(msg)
+						if err != nil {
+							fmt.Println(err)
+							continue
+						}
+						sigBytes, err := privKey.Sign(data)
+						if err != nil {
+							fmt.Println(err)
+							continue
+						}
+						err = sender.Send(&pb.SendReq{
+							MsgBoxId: msgBoxID,
+							MsgCapsule: &pb.MsgCapsule{
+								Msg: msg,
+								Signature: &pb.Signature{
+									PubKey:   pubKeyBytes,
+									SigBytes: sigBytes,
+								},
+							},
+						})
+						if err == io.EOF {
 							return
-						default:
-							data, err := stream.Recv()
-							if err == io.EOF || status.Code(err) > codes.OK {
-								return
-							}
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-							msgCapsule := data.GetMsgCapsule()
-							signature := msgCapsule.GetSignature()
-							if bytes.Equal(signature.GetPubKey(), pubKeyBytes) {
-								continue
-							}
-							msg := msgCapsule.GetMsg()
-							metadata := msg.GetMetadata()
-							nickname := "unknown"
-							value, exist := metadata["nickname"]
-							if exist {
-								nickname = string(value)
-							}
-							if debugEnabled {
-								fmt.Printf("[%s]%s:%s\n", topic, nickname, msg.GetData())
-							}
+						}
+						if err != nil {
+							fmt.Println(err)
+							continue
 						}
 					}
-				}()
+				}
+			}()
 
-				// execute goroutine (sender)
-				subWg.Add(1)
-				go func() {
-					defer subWg.Done()
-					ticker := time.NewTicker(1 * time.Second)
-					defer ticker.Stop()
-					sendClient, err := cli.Send(ctx)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-					for {
-						select {
-						case <-ctx.Done():
-							res, err := sendClient.CloseAndRecv()
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-							if !res.Ok {
-								fmt.Println("failed to send msg")
-								return
-							}
-							return
-						case <-ticker.C:
-							msg := &pb.Msg{
-								Data: []byte(fmt.Sprintf("helloworld-%d", time.Now().UnixNano())),
-								Metadata: map[string][]byte{
-									"nickname": []byte(nickname),
-								},
-							}
-							data, err := proto.Marshal(msg)
-							if err != nil {
-								fmt.Println(err)
-								continue
-							}
-							sigBytes, err := privKey.Sign(data)
-							if err != nil {
-								fmt.Println(err)
-								continue
-							}
-
-							err = sendClient.Send(&pb.SendReq{
-								MsgBoxId: topic,
-								MsgCapsule: &pb.MsgCapsule{
-									Msg: msg,
-									Signature: &pb.Signature{
-										PubKey:   pubKeyBytes,
-										SigBytes: sigBytes,
-									},
-								},
-							})
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
+			// recv
+			subWg.Add(1)
+			go func() {
+				subWg.Done()
+				receiver, err := cli.Recv(ctx, &pb.RecvReq{
+					MsgBoxId:   msgBoxID,
+					ConsumerId: nickname,
+				})
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				for {
+					select {
+					case <-ctx.Done():
+						err = receiver.CloseSend()
+						if err != nil {
+							fmt.Println(err)
 						}
+						return
+					default:
+						data, err := receiver.Recv()
+						if err == io.EOF {
+							return
+						}
+						data.GetMsgCapsule()
 					}
-				}()
-			}
-			time.Sleep(1 * time.Millisecond)
+				}
+			}()
 		}
-		fmt.Printf("successfully initiated clients: %d\n", numOfTopics*numOfUsers)
+
+		fmt.Printf("successfully initiated clients: %d, msgBoxID: %s\n", numOfUsers, msgBoxID)
 		mainWg.Wait()
 		return nil
 	},
@@ -241,7 +217,6 @@ var Cmd = &cobra.Command{
 func init() {
 	Cmd.Flags().BoolVarP(&tlsEnabled, "tls", "t", DefaultTlsEnabled, "enable tls connection")
 	Cmd.Flags().StringVar(&hostAddr, "host", DefaultHostAddr, "host addr")
-	Cmd.Flags().IntVarP(&numOfTopics, "topics", "n", DefaultNumOfTopics, "number of topics")
 	Cmd.Flags().IntVarP(&numOfUsers, "users", "u", DefaultNumOfUsers, "number of users")
 	Cmd.Flags().IntVarP(&topicLength, "length", "l", DefaultTopicLength, "topic length")
 	Cmd.Flags().BoolVarP(&randomEnabled, "random", "r", DefaultRandomEnabled, "enable random topic, nickname")
