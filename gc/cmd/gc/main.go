6a3decfb185a90fe5f866e02de972507717c4002
h0n9 <monsieur.ahn@gmail.com>
2025-06-16 20:31:31 +0900

    chore: Write skeleton with comments

---
+++ gc/cmd/gc/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// read diff from `git diff`
+
+	// ask ai models (e.g., `ollama`) to write git commit messages with the diff
+
+	// print
+
+	// TODO: write commit messages to `git commit`
+}
